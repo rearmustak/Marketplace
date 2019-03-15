030d413014d5cc985c5ac93b78cdc6f302c4bc3e
Aileen Nowak <AileenCGN@gmail.com>
2019-03-15 14:08:47 +0800

    ⬆️  Update dependencies
    no issue
    
    - Update Gulp to v4 incl. Gulfile amendments
    - Update Gems, as `nokogiri` reported a vulnerability

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp = require('gulp');
+const { series, watch, src, dest, parallel } = require('gulp');
+const pump = require('pump');
 
 // gulp plugins and utils
-var gutil = require('gulp-util');
 var livereload = require('gulp-livereload');
 var postcss = require('gulp-postcss');
-var sourcemaps = require('gulp-sourcemaps');
+var beeper = require('beeper');
 
 // postcss plugins
 var autoprefixer = require('autoprefixer');
@@ -13,42 +13,40 @@ var cssnano = require('cssnano');
 var customProperties = require('postcss-custom-properties');
 var easyimport = require('postcss-easy-import');
 
-var swallowError = function swallowError(error) {
-    gutil.log(error.toString());
-    gutil.beep();
-    this.emit('end');
+function serve(done) {
+    livereload.listen();
+    done();
+}
+
+const handleError = (done) => {
+    return function (err) {
+        if (err) {
+            beeper();
+        }
+        return done(err);
+    };
 };
 
-var nodemonServerInit = function () {
-    livereload.listen(1234);
-};
-
-gulp.task('build', ['css'], function (/* cb */) {
-    return nodemonServerInit();
-});
-
-gulp.task('css', function () {
+function css(done) {
     var processors = [
         easyimport,
-        customProperties,
+        customProperties({ preserve: false }),
         colorFunction(),
-        autoprefixer({browsers: ['last 2 versions']}),
+        autoprefixer({ browsers: ['last 2 versions'] }),
         cssnano()
     ];
 
-    return gulp.src('assets/css/*.css')
-        .on('error', swallowError)
-        .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('assets/built/'))
-        .pipe(livereload());
-});
+    pump([
+        src('assets/css/*.css', { sourcemaps: true }),
+        postcss(processors),
+        dest('assets/built/', { sourcemaps: '.' }),
+        livereload()
+    ], handleError(done));
+}
 
-gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['css']);
-});
+const watcher = () => watch('assets/css/**', css);
+const build = series(css);
+const dev = series(build, serve, watcher);
 
-gulp.task('default', ['build'], function () {
-    gulp.start('watch');
-});
+exports.build = build;
+exports.default = dev;
